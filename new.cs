91ded2a981e5bae60589e66901b7735339a3127c
Vik13Rao <vikramrao13@gmail.com>
2022-07-29 17:45:17 +0530

    Update new.cs

--- new.cs
+++ new.cs
@@ -1,4 +1,4 @@
-int Salary = 10;
-float ApproxSalary = 10.0;
-const int Salary =
-const float ApproxSalary
+int Salary=10;
+float ApproxSalary=10.0;
+const int Salary=10;
+const float ApproxSalary=10.0;
