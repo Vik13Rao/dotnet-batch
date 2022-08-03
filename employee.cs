56bc5eb71d914c8b0720507ec1d61a266584a371
Vik13Rao <vikramrao13@gmail.com>
2022-08-03 16:29:02 +0530

    Employee details

---
+++ employee.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace employee
+{
+    class test
+    {
+        public static void Main(string[] args)
+        {
+            string name1= "Vikram Rao";
+            string addr1= "Kandivali";
+            string name2= "John Cena";
+            string addr2= "Usa";
+            employee.emp.list(name1, addr1, name2, addr2);
+
+        }
+    }
+
+    public class emp
+    {
+        public static void list(string str1,string addr1, string str2, string addr2)
+        {
+            string details1, details2;
+            details1 = str1+" "+addr1+" ";
+            details2 = str2+" "+addr2;
+            Console.WriteLine("Emp Details:");
+            Console.WriteLine("1."+details1);
+            Console.WriteLine("2."+details2);
+
+        }
+    }
+    }
