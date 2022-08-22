3457a93665eedd6b4231fb44e52dfef5be94e9c3
Vik13Rao <vikramrao13@gmail.com>
2022-08-22 17:07:36 +0530

    functional component commit

--- React-HandsOn/vikram/src/App.js
+++ React-HandsOn/vikram/src/App.js
@@ -9,12 +9,17 @@ import { ReactDOM } from 'react-dom/client';
 function App(){
   return <div>
     <AppChild name="Matt" />
+    <App2 name = "Successful"/>
     </div>
 }
 function AppChild(props){
   return <span>
       My name is {props.name}
     </span>
+}
+
+function App2(props){
+  return <p>Functional component : {props.name} </p>
 }
 // ReactDOM.render(<App />, document.getElementById('app'));
 
