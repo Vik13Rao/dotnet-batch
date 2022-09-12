3abd504d613b9aef89699a7734d7e0c1b0906d14
Vik13Rao <vikramrao13@gmail.com>
2022-09-12 19:40:42 +0530

    Webservice commit

---
+++ WebService/ConsumeMathService/ConsumeMathService/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumeMathService.localhost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {localhost.MathService myMathService = new localhost.MathService();
+            Console.Write($"2 * 4 = {myMathService.Multiply(2, 4)}\n");
+            Console.Write($"8 % 2 = {myMathService.Divide(8, 2)}");
+        }
+    }
+}
---
+++ WebService/SecondService/SecondService/MathService.asmx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace SecondService
+{
+    /// <summary>
+    /// Summary description for SecondService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class MathService : System.Web.Services.WebService
+    {
+        //[WebMethod(Description = "Method of running web service")]
+        //public string show()
+        //{
+        //    return "Method from second web service";
+        //}
+        //[WebMethod(Description = "Method of called web service")]
+        //public string display()
+        //{
+        //    localhost.MathService obj = new localhost.MathService();
+        //    return obj.show();
+        //}
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+        [WebMethod(Description = "Method of called web service")]
+        public string display()
+        {
+            localhost.MathService myMath = new localhost.MathService();
+            return myMath.HelloWorld();
+        }
+    }
+}
